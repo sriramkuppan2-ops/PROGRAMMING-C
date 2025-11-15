8173dd699ec2aad7c00bea66e316eaff6c9983ad
SRIRAM K <sriramkuppan2@gmail.com>
2025-11-15 10:45:39 +0530

    Add files via upload

---
+++ P1.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, simple_interest;
+
+    printf("Enter the principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter the rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter the time (in years): ");
+    scanf("%f", &time);
+
+    simple_interest = (principal * rate * time) / 100;
+
+    printf("Simple Interest = %.2f\n", simple_interest);
+
+    return 0;
+}
