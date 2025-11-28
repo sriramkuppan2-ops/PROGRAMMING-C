91a1d03559b0c1b5691640cba5c1af9aec10fe48
SRIRAM K <sriramkuppan2@gmail.com>
2025-11-28 08:00:04 +0530

    Add files via upload

---
+++ P12.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter an alphabet: ");
+    scanf("%c", &ch);
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        if (ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            printf("%c is a vowel.\n", ch);
+        else
+            printf("%c is a consonant.\n", ch);
+    } else
+        printf("Invalid input.\n");
+    return 0;
+}
